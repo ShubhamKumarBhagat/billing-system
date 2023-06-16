f22cad927ceefda686510e8316b61126ae010158
Shubham Kumar Bhagat <shubhamkumarbhagat01@gmail.com>
2023-06-16 08:30:48 +0530

    import offering endpoints

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ const app=express()
 const port=3000
 require("./db/mongoose")
 
+
+const offeringRouter=require("./routers/offering")
+
+
+
 app.use(express.json())
+app.use(offeringRouter)
 
 app.get("/",(req,res)=>{
     console.log(req.body)
