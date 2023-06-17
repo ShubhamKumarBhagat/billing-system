02c8e78980f76858138bc870c206bcc3f9f709d6
Shubham Kumar Bhagat <shubhamkumarbhagat01@gmail.com>
2023-06-17 16:12:22 +0530

    Add cart router

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ require("./db/mongoose")
 const offeringRouter=require("./routers/offering")
 const orderRouter=require("./routers/order")
 const userRouter=require("./routers/user")
+const cartRouter=require("./routers/cart")
 
 
 app.use(express.json())
 app.use(offeringRouter)
 app.use(orderRouter)
 app.use(userRouter)
+app.use(cartRouter)
 
 app.get("/",(req,res)=>{
     console.log(req.body)
