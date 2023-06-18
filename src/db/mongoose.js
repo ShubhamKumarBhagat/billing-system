8bd09bc6656f8e939b4b3369a3ff3d01f23e5ca5
Shubham Kumar Bhagat <shubhamkumarbhagat01@gmail.com>
2023-06-19 00:50:42 +0530

    modify connection url to connect to mongo atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,16 @@
 const mongoose=require("mongoose")
 
-const MONGO_URL='mongodb://127.0.0.1:27017/billing-system'
+// const MONGO_URL='mongodb://127.0.0.1:27017/billing-system'
+
+const MONGO_URL='mongodb+srv://shubham:QAzs8M4LgkMk9Mn3@cluster0.iy9clp2.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(MONGO_URL,{
     useNewUrlParser:true,
 }).then((res)=>{
-    console.log(res)
+    
+    
+    console.log("Connection to DB established...")
+    
+
 })
 .catch((err)=>{console.log(err)})
