3d0decfd26a030911107e46d1a74243e52ec3e26
Shubham Kumar Bhagat <shubhamkumarbhagat01@gmail.com>
2023-06-19 00:51:16 +0530

    add admin router

--- src/index.js
+++ src/index.js
@@ -8,18 +8,15 @@ const offeringRouter=require("./routers/offering")
 const orderRouter=require("./routers/order")
 const userRouter=require("./routers/user")
 const cartRouter=require("./routers/cart")
-
+const adminRouter=require("./routers/admin")
 
 app.use(express.json())
 app.use(offeringRouter)
 app.use(orderRouter)
 app.use(userRouter)
 app.use(cartRouter)
+app.use(adminRouter)
 
-app.get("/",(req,res)=>{
-    console.log(req.body)
-    res.send('Hello World')
-})
 
 app.listen(port,()=>{
     console.log("Server is running on port",port)
